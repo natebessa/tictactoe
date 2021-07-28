0bee97e6a30ea8cdd6a91e06c00ae4aa7c246a57
Nate Bessa <natebessa@gmail.com>
2021-07-27 19:21:32 -0600

    Replace hard-coded rendering of squares with two loops
    Challenge #3 in the ReactJS tic-tac-toe tutorial.

--- src/index.js
+++ src/index.js
@@ -23,21 +23,13 @@ class Board extends React.Component {
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {Array(3).fill(null).map((element, i) =>
+                    <div className="board-row">
+                        {Array(3).fill(null).map((element, j) =>
+                            this.renderSquare(j + (i * 3))
+                        )}
+                    </div>
+                )}
             </div>
         );
     }
